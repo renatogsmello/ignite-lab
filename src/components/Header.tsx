50535049aa46e6b624c46f7310316f95ba0ef6d7
renato.mello <renato.mello@globalhitss.com.br>
2022-06-22 16:49:36 -0300

    lesson 02

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+	return (
+		<>
+			<header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+				<span className="font-bold border-emerald-600 border-r-4 px-2 text-2xl">Ignite Lab</span>
+				<span className="uppercase text-gray-400 px-2 text-lg">reactjs</span>
+			</header>
+		</>
+	)
+}
